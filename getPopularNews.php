866bdb1484f2e8bcef416dd508046582d7af730a
FilipeJohasson <filipejohansson7@gmail.com>
2021-09-21 22:06:19 -0300

    proving all data to popular and trending

---
+++ getPopularNews.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("./Carousel.php");
+
+$news[] = new Carousel(
+    'http://placehold.it/760x400/55555/cccccc', 
+    '[POPULAR] Lorem ipsum dolor sit amet, consectetur adipiscing elit', 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et finibus dolor. Mauris a felis ac ex pharetra interdum eu eget dui.'
+);
+
+$news[] = new Carousel(
+    'http://placehold.it/760x400/5i5i5/cccccc', 
+    '[POPULAR] Fusce et finibus dolor. Mauris a felis ac ex pharetra interdum eu eget dui.', 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et finibus dolor. Mauris a felis ac ex pharetra interdum eu eget dui.'
+);
+
+$news[] = new Carousel(
+    'http://placehold.it/760x400/9a1j2o/cccccc', 
+    '[POPULAR] Nam eget interdum metus, et porttitor libero.', 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et finibus dolor. Mauris a felis ac ex pharetra interdum eu eget dui.'
+);
+
+$news[] = new Carousel(
+    'http://placehold.it/760x400/999999/cccccc', 
+    '[POPULAR] Aliquam blandit vel arcu quis ultrices. Sed pellentesque tincidunt velit, non tincidunt enim maximus quis.', 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et finibus dolor. Mauris a felis ac ex pharetra interdum eu eget dui.'
+);
+
+$news[] = new Carousel(
+    'http://placehold.it/760x400/dddddd/333333', 
+    '[POPULAR] Donec rhoncus, risus in convallis ultrices, metus mauris vulputate ante, nec laoreet enim ex a ante.', 
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce et finibus dolor. Mauris a felis ac ex pharetra interdum eu eget dui.'
+);
+
+echo json_encode($news);
